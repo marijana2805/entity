66280747a5adcccd8c934537e041e761c0332580
Marijana <masadjurnic@gmail.com>
2020-03-11 17:24:23 +0100

    before json server

--- src/app/entities/entities.component.ts
+++ src/app/entities/entities.component.ts
@@ -12,12 +12,19 @@ export class EntitiesComponent implements OnInit {
   entity = Entity;
   searchName: string;
   newArr: any;
-  selected: boolean;
+  private selectedListId: any;
 
   constructor(private entitiesService: EntitiesService) {
   }
 
   ngOnInit() {
+    this.entity.map(x => x.selected = false);
+    if (this.entitiesService.saved.length > 0) {
+      this.selectedListId = this.entitiesService.saved.map(x => x.id); // izvuci id-ove cekiranih iz niza saved
+      this.entity.filter(x => this.selectedListId.includes(x.id)).map(x => x.selected = true);
+      // ?! id iz entiteta koji se poklapa sa id iz selectedListId
+      // provjerava da li je id iz entitteta 'ukljucen' u  selectedListId
+    }
   }
 
   onSave(opt: MatListOption[]) {
