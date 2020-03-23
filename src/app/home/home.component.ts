9ddbe82d404a1b00dcc4d22371c2fea26477809e
Marijana <masadjurnic@gmail.com>
2020-03-23 10:20:14 +0100

    entity

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,9 @@ import {ConnectService} from '../connect.service';
 })
 export class HomeComponent implements OnInit {
   selectedList: List[] = [];
+  public show = false;
+  buttonname: any = `keyboard_arrow_down`;
+  public icon = false;
 
   public constructor(private entitiesService: EntitiesService,
                      private connectService: ConnectService) {
@@ -21,5 +24,14 @@ export class HomeComponent implements OnInit {
 
   getConnected(): void {
     this.connectService.getChecked().subscribe(selectedList => this.selectedList = selectedList);
+  }
+
+  toggle() {
+    this.show = !this.show;
+    if (this.show) {
+      this.buttonname = `keyboard_arrow_up`;
+    } else {
+      this.buttonname = `keyboard_arrow_down`;
+    }
   }
 }
